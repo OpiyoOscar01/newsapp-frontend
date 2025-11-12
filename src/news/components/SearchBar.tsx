472b783cc37433a9f5901e76ade308d72a34e866
OPIYO OSCAR <opiyooscar414@gmail.com>
2025-11-12 14:47:38 +0300

    Build production files.

--- src/news/components/SearchBar.tsx
+++ src/news/components/SearchBar.tsx
@@ -52,7 +52,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
         <div className="relative flex-1">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <svg
-              className="h-5 w-5 text-gray-400"
+              className="h-5 w-5 text-gray-400 pl-2"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
